3452bc1868a5311a8ae0a5f5dabb4d5529e23a75
Kasper Scott Jensen <kasp324f@stud.kea.dk>
2022-03-31 19:43:10 +0200

    index

--- src/main/resources/javascripts/index.js
+++ src/main/resources/javascripts/index.js
@@ -1,17 +1,22 @@
+//runs when window loads
 window.onload = function() {
 
+    //variables
     var i = 1;
     var seconds = document.getElementById('waste');
 
+    //sets run-interval to 1 second
     function startCount() {
         setInterval(increase, 1000)
     }
 
+    //adds to variable i and prints to html
     function increase() {
         i++;
         seconds.innerHTML = i * 21;
     }
 
+    //run
     startCount();
 
 }
