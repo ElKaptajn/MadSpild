464c13f9b02de2718c149e1408b07bd086fc3675
breelef <emil_vinther@hotmail.com>
2022-04-06 12:09:52 +0200

    Commercial fin puds

--- src/main/resources/javascripts/Resultat.js
+++ src/main/resources/javascripts/Resultat.js
@@ -8,7 +8,7 @@ window.onload = function() {
     let x = document.getElementById("result").value;
     let y = 5*(x);
     let z = 365*(y);
-    let t = 0.5*(x);
+    let t = 0.12*(x);
     let s = 365*(t);
     let dagDKK = document.getElementById('dagDKK').innerText = y;
     let yearDKK = document.getElementById("yearDKK").innerText = z;
