3df5f8ef69e9a952df7e9457f6657e13c54e9761
breelef <emil_vinther@hotmail.com>
2022-04-06 11:30:27 +0200

    Commercial calculator

---
+++ src/main/resources/javascripts/Resultat.js
@@ -0,0 +1,18 @@
+
+window.onload = function() {
+    var btn = document.getElementById("btn");
+    btn.addEventListener("click", solve);
+}
+
+    function solve(){
+    let x = document.getElementById("result").value;
+    let y = 5*(x);
+    let z = 365*(y);
+    let t = 1.5*(x);
+    let s = 365*(t);
+    let dagDKK = document.getElementById('dagDKK').innerText = y;
+    let yearDKK = document.getElementById("yearDKK").innerText = z;
+    let dagKG = document.getElementById("dagKG").innerText = t;
+    let yearKG = document.getElementById("yearKG").innerText = s;
+}
+
