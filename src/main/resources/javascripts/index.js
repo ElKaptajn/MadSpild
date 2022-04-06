13f882dcff780dc78953d7a49925cf653f57574f
Kasper Scott Jensen <kasp324f@stud.kea.dk>
2022-04-06 09:35:02 +0200

    homecontroller updated and pictures

--- src/main/resources/javascripts/index.js
+++ src/main/resources/javascripts/index.js
@@ -1,22 +1,17 @@
-//runs when window loads
 window.onload = function() {
 
-    //variables
-    let i = 1;
-    let seconds = document.getElementById('waste');
+    let s = 1;
+    let waste = document.getElementById('waste');
 
-    //sets run-interval to 1 second
+    function increase() {
+        s++;
+        waste.innerText = s * 21;
+    }
+
     function startCount() {
         setInterval(increase, 1000)
     }
 
-    //adds to variable i and prints to html
-    function increase() {
-        i++;
-        seconds.innerText = i * 21;
-    }
-
-    //run
     startCount();
 
 }
